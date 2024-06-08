7006df0945a8b585c176dd982e331420d4cd89c4
Rohen Agarwal <70237628+raa5@users.noreply.github.com>
2024-06-08 17:28:07 -0500

    Add files via upload

---
+++ DistanceApp/google_maps.py
@@ -0,0 +1,35 @@
+import requests
+import smtplib
+
+# API Key
+api_file = open("api-key.txt", "r")
+api_key = api_file.read()
+api_file.close()
+
+# Home address input
+# home = input("Enter home address\n")
+dummy_home = '406 Greenbriar Dr, Normal'
+
+# Work address input
+# work = input("Enter work address\n")
+dummy_work = '100 Rivian Motorway, Normal'
+
+
+# Base URL
+url = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&'
+
+# Get response
+r = requests.get(url + "origins=" + dummy_home + "&destinations=" + dummy_work + "&key=" + api_key)
+# print(r)
+
+# Return time as text and as seconds
+time = r.json()["rows"][0]["elements"][0]["duration"]["text"]
+seconds = r.json()["rows"][0]["elements"][0]["duration"]["value"]
+
+distance = r.json()["rows"][0]["elements"][0]["distance"]["text"]
+miles = r.json()["rows"][0]["elements"][0]["distance"]["value"]
+
+# print(r.json())
+# Print the total travel time
+# print("\nThe total travel time from home to work is = ", time)
+print('\n The distance between ' + dummy_home + ' and ' + dummy_work + ' is ' + distance + '\n It will take ' + time)
